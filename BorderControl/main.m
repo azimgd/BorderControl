1cc15764bb7c1ca9690f14544dcee8a2996fcac1
azimgd <me@azimgd.com>
2023-06-25 15:08:14 +0500

    remove obsolete networkcommunication files

--- BorderControl/main.m
+++ BorderControl/main.m
@@ -7,20 +7,11 @@
 
 #import <Cocoa/Cocoa.h>
 #import "NetworkExtension.h"
-#import "NetworkCommunication.h"
 #import "ExtensionBundle.h"
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
     [[NetworkExtension shared] install];
-    
-    NSBundle *extensionBundle = [[ExtensionBundle shared] extensionBundle:[NSBundle mainBundle]];
-    NSString *machService = [[ExtensionBundle shared] extensionBundleMachService:extensionBundle];
-    [[NetworkCommunication shared] startConnection:machService callback:^(NSError *error) {
-      [[NetworkCommunication shared] dispatcher:@"#1" callback:^(NSString *response) {
-        NSLog(@"[border-control] dispatched correctly #0");
-      }];
-    }];
   }
   return NSApplicationMain(argc, argv);
 }
--- Network/main.m
+++ Network/main.m
@@ -7,19 +7,12 @@
 
 #import <Foundation/Foundation.h>
 #import <NetworkExtension/NetworkExtension.h>
-#import "NetworkCommunication.h"
 #import "ExtensionBundle.h"
 
 int main(int argc, char *argv[])
 {
   @autoreleasepool {
     [NEProvider startSystemExtensionMode];
-    NSString *machService = [[ExtensionBundle shared] extensionBundleMachService:[NSBundle mainBundle]];
-    [[NetworkCommunication shared] startListener:machService callback:^(NSError *error) {
-      [[NetworkCommunication shared] dispatcher:@"#1" callback:^(NSString *response) {
-        NSLog(@"[border-control] dispatched correctly #1");
-      }];
-    }];
   }
 
   dispatch_main();
