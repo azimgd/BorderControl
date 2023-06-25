917b1283c74c997a53bb37bb3f3ea9459003a090
azimgd <me@azimgd.com>
2023-06-25 15:08:14 +0500

    Initial Commit

---
+++ BorderControl/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  BorderControl
+//
+//  Created by azimgd on 13.06.2023.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+  [super setRepresentedObject:representedObject];
+
+  // Update the view, if already loaded.
+}
+
+
+@end
---
+++ BorderControl/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  BorderControl
+//
+//  Created by azimgd on 13.06.2023.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
