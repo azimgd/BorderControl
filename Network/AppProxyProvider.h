13d8aeca2321628ca0f7183cdeaa39cdf6b42c26
azimgd <me@azimgd.com>
2023-06-25 15:08:14 +0500

    project targets setup

---
+++ Network/AppProxyProvider.h
@@ -0,0 +1,12 @@
+//
+//  AppProxyProvider.h
+//  Network
+//
+//  Created by azimgd on 13.06.2023.
+//
+
+#import <NetworkExtension/NetworkExtension.h>
+
+@interface AppProxyProvider : NEAppProxyProvider
+
+@end
